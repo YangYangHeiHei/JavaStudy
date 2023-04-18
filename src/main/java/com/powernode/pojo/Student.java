69050ab978b246e1e73faba3f8be464b7331ff4f
Yang <888@163.com>
2023-04-18 21:47:28 +0800

    Add POJOStudent

---
+++ src/main/java/com/powernode/pojo/Student.java
@@ -0,0 +1,12 @@
+package com.powernode.pojo;
+
+import lombok.Data;
+
+@Data
+public class Student {
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+    private String address;
+}
